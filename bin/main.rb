2e0b14eba8cd9040db8d1c42095174ff621fe678
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-07-14 10:20:37 +0100

    Prompt player to add moves

--- bin/main.rb
+++ bin/main.rb
@@ -14,10 +14,23 @@ class TicTacToe
     puts "#{@name2} is #{@player2}"
   end
 
-  def play
-    puts 'Make a move?'
-    # first_move = gets.chomp.strip
-    @board = Board.new
+
+  def player_turn
+    count = 0
+    9.times do 
+      if count%2 == 0
+        player = @name1
+        puts "#{player} Make a move?Choose from 1-9"
+        move = gets.chomp.strip 
+        @board.draw_board
+      else 
+        player = @name2 
+        puts "#{player} Make a move?Choose from 1-9"
+        move = gets.chomp.strip 
+        @board.draw_board
+      end
+      count +=1
+    end   
   end
 
   def winner
@@ -27,17 +40,17 @@ class TicTacToe
   def draw
     puts 'It is a tie'
   end
-
-  def player_again
-    puts 'Do you want continue?yes or no'
-    # response = get.chomp.strip
-  end
+ 
 end
 
 # Display the Board
 class Board
   def initialize
     @position = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+  
+  def draw_board
+    puts '------------------'
     puts "|  #{@position[0]}  |  #{@position[1]}  |  #{@position[2]} |"
     puts '------------------'
     puts "|  #{@position[3]}  |  #{@position[4]}  |  #{@position[5]} |"
@@ -45,4 +58,15 @@ class Board
     puts "|  #{@position[6]}  |  #{@position[7]}  |  #{@position[8]} |"
     puts '------------------'
   end
+
+  def update_board(move)
+    move = @position[i]
+  end
 end
+
+ play = TicTacToe.new
+ play.player_turn
+# display = Board.new
+# display.player_turn
+
+
