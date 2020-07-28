b852934b890128a68c9045ddddd477f2d86b1a2d
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-07-28 12:03:43 +0100

    Add test for update_board method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,13 @@
 require './lib/game.rb'
+require './lib/player.rb'
 
 describe GameLogic do
   let(:my_result) {  }
+  let(:move) { 0 }
   game = GameLogic.new
+  game.position
+  game.player1 = Player.new(name, 'X')
+  game.player2 = Player.new(name, 'O')
   describe '#board' do
     let(:str) { "\n    |  7  |  8  |  9 |\n    ------------------\n    |  4  |  5  |  6 |\n    ------------------\n    |  1  |  2  |  3 |\n    ------------------" }
     it "returns the board string" do
@@ -15,6 +20,13 @@ describe GameLogic do
     it "returns player1 or Player 2" do
       expect(game.switch_player).to eql(1)
       expect(game.switch_player).to eql(0)
+    end
+  end
+
+  describe "#update_board" do
+    it "returns a mark to update the board" do
+      expect(game.player1.mark).to eql('X')
+      expect(game.player2.mark).to eql('O')
     end
   end
 end
